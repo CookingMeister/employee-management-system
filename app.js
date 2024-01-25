5d919e821c18732ee0a0e3c7656115194512c7c5
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-01-25 12:42:13 -0400

    create schema for db and tables

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const validator = require('validator');
-
+const inquirer = require("inquirer");
+// const validator = require("validator");
 
 // Middleware
 // app.use(bodyparser.json());
@@ -42,11 +41,12 @@ const validator = require('validator');
 //   });
 // });
 
-// Prompt user for input
-async function promptUser() {
-  try {
+
+  // Prompt user for input
+  async function promptUser() {
     let repeat = true;
     while (repeat) {
+    try {
       const answers = await inquirer.prompt([
         {
           type: "list",
@@ -64,41 +64,54 @@ async function promptUser() {
           ],
         },
       ]);
-      // Switch Case Logic
-      switch (answers.menu) {
-        case "View All Employees":
-          console.log("View All Employees logic");
-          break;
-        case "View All Roles":
-          console.log("View All Roles logic");
-          break;
-        case "View All Departments":
-          console.log("View All Departments logic");
-          break;
-        case "Add Employee":
-          console.log("Add employees logic");
-          break;
-        case "Add Role":
-          console.log("Add roles logic");
-          break;
-        case "Add Department":
-          console.log("Add department logic");
-          break;
-        case "Update Employee Role":
-          console.log("Update employee role logic");
-          break;
-        case "EXIT":
-          console.log("Thank you for using the employee management system!");
-          repeat = false;
-          break;
-
-        default:
-          return "invalid selection";
-      }
+      checkAnswer(answers);
+    } catch (error) {
+      console.log(error);
     }
-  } catch (err) {
-    console.log("oops", err);
-    throw err;
   }
+
+  function checkAnswer(answers) {
+    // Switch Case Logic
+    switch (answers.menu) {
+      case "View All Employees":
+        console.log("View All Employees logic");
+        // Query database to select all employees
+        break;
+
+      case "View All Roles":
+        console.log("View All Roles logic");
+        // Query database to select all roles
+        break;
+
+      case "View All Departments":
+        console.log("View All Departments logic");
+        // Query database to select all departments
+        break;
+
+      case "Add Employee":
+        console.log("Add employees logic");
+        break;
+
+      case "Add Role":
+        console.log("Add roles logic");
+        break;
+
+      case "Add Department":
+        console.log("Add department logic");
+        break;
+
+      case "Update Employee Role":
+        console.log("Update employee role logic");
+        break;
+
+      case "EXIT":
+        console.log("Thank you for using the employee management system!");
+        repeat = false; // break while loop
+        break;
+
+      default:
+        throw new Error("Invalid selection");
+    }
+  }  
 }
 promptUser();
