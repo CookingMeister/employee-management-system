04bbc34ca9aa6ed3d2d32350c292d3e7b0b34ade
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-01-26 17:31:05 -0400

    move db config to config.js, move query functions to utils.js,import/export, working

--- app.js
+++ app.js
@@ -1,64 +1,19 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+// const consoleTable = require("console.table");
+// const db = require("./lib/utils/config.js");
+const {
+  selectAllEmployees,
+  selectAllDeparments,
+  selectAllRoles
+} = require("./lib/utils/utils.js");
 // require('dotenv').config();
 // require("console.table");
 // const validator = require("validator");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Office2023!",
-  database: "company",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
 
-function selectAllEmployees() {
-  db.query(`SELECT
-  e.id, 
-  e.first_name,
-  e.last_name,
-  r.title,
-  d.name AS department, 
-  r.salary,
-  CONCAT(m.first_name, ' ', m.last_name) AS manager
-FROM employee e
-LEFT JOIN role r ON e.role_id = r.id
-LEFT JOIN department d ON r.department_id = d.id 
-LEFT JOIN employee m ON e.manager_id = m.id
-ORDER BY department, salary DESC;
-`, (err, results) => {
-    if (err) throw err;
-    // console.log("\n");
-    // console.log(consoleTable.getTable(results));
 
-    // Get the table as a string
-    const tableString = consoleTable.getTable(results);
-    // Calculate the width of the table
-    const tableWidth = tableString.split("\n")[0].length;
-    // Calculate the number of spaces needed for centering
-    const spacesBefore = Math.floor((process.stdout.columns - tableWidth) / 2);
-    // Add spaces before the entire table, including headers
-    console.log('\n' + '\n' +
-      " ".repeat(spacesBefore) +
-        tableString.replace(/\n/g, "\n" + " ".repeat(spacesBefore)) + '\n'
-    );
-  });
-}
-function selectAllDeparments() {
-  db.query("SELECT * FROM department", (err, results) => {
-    if (err) throw err;
-    console.log("\n" + consoleTable.getTable(results));
-  });
-}
-function selectAllRoles() {
-  db.query("SELECT * FROM role", (err, results) => {
-    if (err) throw err;
-    console.log("\n" + consoleTable.getTable(results));
-  });
-}
+
 
 
 
